11126a12b5cdc1d948d70db6e7b9cb348832baa4
keyur_patel <keyur.patel@simformsolutions.com>
2024-09-07 11:45:45 -0400

    implemented Excercise 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,20 +1,29 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
+
+  # Time complexity for all operations : O(1)
+  # Space complexity : O(n), where n in the number of elements in the Stack
      def __init__(self):
+       self.stackarr = []
          
      def isEmpty(self):
+       return len(self.stackarr) == 0
          
      def push(self, item):
+       return self.stackarr.append(item)
          
      def pop(self):
-        
+        return self.stackarr.pop()
         
      def peek(self):
+        return self.stackarr[-1]
         
      def size(self):
+        return len(self.stackarr)
          
      def show(self):
+       return self.stackarr
          
 
 s = myStack()
